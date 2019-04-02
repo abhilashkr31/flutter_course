2fb31a2b7d17467f606c3aa97c15fe32dc46b859
Abhilash Kumar <abhilashkr31@gmail.com>
2019-04-02 21:51:27 +0530

    product-manager: Added first files for product widgets
    Created first flutter app with basic layouts to be followed
    and enhanced while making the complete product managing app.

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+import './product_manager.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Easy List"),
+        ),
+        body: ProductManager(),
+      ),
+    );
+  }
+}
