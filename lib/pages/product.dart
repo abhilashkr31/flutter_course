33dc73669ee7949aa97722f07e60167f65714618
Abhilash Kumar <abhilashkr31@gmail.com>
2019-04-13 22:00:13 +0530

    product-manager: Add constructor to pass data around the pages
    Add the method to pass data aroung pages and wodgets using Map.

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  ProductPage(this.title, this.imageUrl);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,16 +15,19 @@ class ProductPage extends StatelessWidget {
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Image.asset('assets/food.jpg'),
+            Image.asset(imageUrl),
             Container(
-                padding: EdgeInsets.all(10.0), child: Text("Product Details!")),
+                padding: EdgeInsets.all(10.0), 
+                child: Text(title),
+            ),
             Container(
                 padding: EdgeInsets.all(10.0),
                 child: RaisedButton(
                   color: Theme.of(context).accentColor,
                   child: Text("Back"),
                   onPressed: () => Navigator.pop(context),
-                )),
+                ),
+            ),
           ],
         ));
   }
