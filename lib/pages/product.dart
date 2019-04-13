1f9a1c4b5f990334df6a2649f8aa69a3b503219e
Abhilash Kumar <abhilashkr31@gmail.com>
2019-04-13 17:41:52 +0530

    product-manager: Add navigation capability to the App
    This change adds navigation capability to the app for
    jumping to new page and coming back to previous one.

---
+++ lib/pages/product.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Product Details"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text("Product Details!"),
+          RaisedButton(
+            child: Text("Back"),
+            onPressed: () => Navigator.pop(context),
+          )
+        ],
+      ),
+    );
+  }
+}
