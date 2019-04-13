4681e01c994f90cb7fb02ef5c3357ee487e60f7f
Abhilash Kumar <abhilashkr31@gmail.com>
2019-04-13 19:57:16 +0530

    product-manager: Beautify the UI a little
    Adjust the widgets and other properties to
    make the UI look good.

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -4,18 +4,23 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Product Details"),
-      ),
-      body: Column(
-        children: <Widget>[
-          Text("Product Details!"),
-          RaisedButton(
-            child: Text("Back"),
-            onPressed: () => Navigator.pop(context),
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Product Details"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/food.jpg'),
+            Container(
+                padding: EdgeInsets.all(10.0), child: Text("Product Details!")),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text("Back"),
+                  onPressed: () => Navigator.pop(context),
+                )),
+          ],
+        ));
   }
 }
