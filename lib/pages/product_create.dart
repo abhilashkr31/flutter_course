707b155411a7c5d21cc2e5e2bbd726cb778591a4
Abhilash Kumar <abhilashkr31@gmail.com>
2019-04-20 21:22:41 +0530

    product-manager: Add user input fields to create product tab
    This change adds fields like title, decription and price that
    can be added for a particular product.

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue = '';
+  String descriptionValue = '';
+  double priceValue = 0.0;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text("Save"),
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Center(
-                  child: Text("This is a Modal!"),
-                );
-              });
-        },
-      ),
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              titleValue = value;
+            });
+          },
+        ),
+        TextField(
+          maxLines: 4,
+          onChanged: (String value) {
+            setState(() {
+              descriptionValue = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.numberWithOptions(),
+          onChanged: (String value) {
+            setState(() {
+              priceValue = double.parse(value);
+            });
+          },
+        ),
+      ],
     );
   }
 }
