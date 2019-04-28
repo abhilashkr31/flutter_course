0875c5d55fbcc6c44caeccf097d569b442976c66
Abhilash Kumar <abhilashkr31@gmail.com>
2019-04-28 16:39:11 +0530

    product-manager: Reorganise code to make more granular
    This change further modifies the App to reorganise the
    code and make it more readable by modularising the same.

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/products/ui_elements/title_default.dart';
+
 class ProductPage extends StatelessWidget {
   final String title;
+  final String description;
+  final double price;
   final String imageUrl;
 
-  ProductPage(this.title, this.imageUrl);
+  ProductPage(this.title, this.imageUrl, this.price, this.description);
 
   _showWarningDialog(BuildContext context) {
     showDialog(
@@ -33,6 +37,32 @@ class ProductPage extends StatelessWidget {
     );
   }
 
+  Widget _buildAddressPriceRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "Union Square, San Francisco",
+          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 5.0),
+          child: Text(
+            '|',
+            style: TextStyle(
+              fontFamily: 'Oswald',
+              color: Colors.grey,
+            ),
+          ),
+        ),
+        Text(
+          "\$$price",
+          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -50,7 +80,15 @@ class ProductPage extends StatelessWidget {
             Image.asset(imageUrl),
             Container(
               padding: EdgeInsets.all(10.0),
-              child: Text(title),
+              child: TitleDefault(title),
+            ),
+            _buildAddressPriceRow(),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                description,
+                textAlign: TextAlign.center,
+              ),
             ),
             Container(
               padding: EdgeInsets.all(10.0),
